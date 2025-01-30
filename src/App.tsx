941e08a9c093d8e5e700ddb2d1b90d062428f129
Jhon Scarzo <jhon753951@gmail.com>
2025-01-30 23:26:24 +0000

    cleanup and updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import './App.css';
-import { Box } from '@mui/material';
 import BasePage from './components/BasePage';
 
 const App = () => {
   return (
-    <Box>
+    <>
       <BasePage />
-    </Box>
+    </>
   );
 }
 
--- src/components/BasePage.tsx
+++ src/components/BasePage.tsx
@@ -1,18 +1,36 @@
-import { Box, Grid } from "@mui/material";
+import { AppBar, Box, Button, Grid, IconButton, Toolbar, Typography } from "@mui/material";
 import PlayerSearchCard from "./PlayerSearchCard";
+import GHLogo from "../assets/github-mark.svg"
 
 const BasePage = () => {
   return (
-    <Box>
-      <Grid container>
-        <Grid item xs={5}>
-          <PlayerSearchCard />
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            CT Game Count
+          </Typography>
+          <img
+            src={GHLogo}
+            alt="Github"
+            height={"24px"}
+            width={"24px"}
+            onClick={() => window.open("https://github.com/ScarzoJ/CTGameCount/tree/gh-pages", "Github")}
+            style={{cursor:"pointer"}}
+          />
+        </Toolbar>
+      </AppBar>
+      <Box>
+        <Grid container>
+          <Grid item xs={5}>
+            <PlayerSearchCard />
+          </Grid>
+          <Grid item xs={5}>
+            <PlayerSearchCard />
+          </Grid>
         </Grid>
-        <Grid item xs={5}>
-          <PlayerSearchCard />
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 }
 
